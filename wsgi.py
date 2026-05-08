6a577118a4d6bcc1958a98f609b490bded5fedd9
Jeaustin Rodriguez E <justicon7@gmail.com>
2026-05-07 21:59:39 -0600

    PRODUCCION

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+# =====================================================
+# WSGI ENTRYPOINT - ASERVE
+# Archivo usado por servidores de producción
+# como Gunicorn, Waitress, Render, Railway, VPS, etc.
+# =====================================================
+from app import app
